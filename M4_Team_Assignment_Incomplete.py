41f34d036731fbe8b332c4545394e760a653c7fb
cmm616 <cmiles8@my.nl.edu>
2024-10-16 16:29:03 -0500

    Add files via upload

---
+++ M4_Team_Assignment_Incomplete.py
@@ -0,0 +1,14 @@
+#Module 4: Team Assignment
+
+
+
+print("You just woke up, how are you going to start your day?") 
+print("2. Make your bed")
+print("3. Brush your teeth")
+print("4. Clean your room")
+print("5. Do push ups")
+print("6. Stretch")
+print("7. Make breakfast")
+print("8. Shower")
+print("9. Get dressed")
+
